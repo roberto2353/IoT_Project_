6775c0e97e954cdb359f610f814b30535bb1cc14
keving-ing <s273302@studeti.polito.it>
2024-07-22 11:16:03 +0200

    Resource_catalog_ok

---
+++ DEVICE_CONNECTOR/sensor.py
@@ -0,0 +1,69 @@
+import cherrypy
+import requests
+import json
+import random
+import time
+import threading
+
+class SensorREST(threading.Thread):
+    exposed = True
+    devices_counter = 1
+    def __init__(self, pi):
+        threading.Thread.__init__(self)
+        self.settings = json.load(open('settings.json'))
+        self.catalogURL=self.settings['catalogURL']
+        self.devices = self.settings['devices']
+        #self.deviceInfo=self.settings['deviceInfo']
+        #self.deviceInfo['ID'] = random.randint(1, 1000)
+        #self.deviceInfo['commands'] = ['hum', 'temp']
+        self.pingInterval = pi
+        #requests.post(f'{self.catalogURL}/devices', data=json.dumps(self.deviceInfo))
+        self.deviceInfo = {} 
+        self.register_devices()
+        self.start()
+
+    def register_devices(self):
+        for device in self.devices:
+            device_info = device['deviceInfo']
+            #device_info['ID'] = random.randint(1, 1000)
+            device_info['ID'] = SensorREST.devices_counter
+            SensorREST.devices_counter += 1 
+            if device_info['type'] == 'photocell':
+                device_info['commands'] = ['status']
+            requests.post(f'{self.catalogURL}/devices', data=json.dumps(device_info))
+
+    def GET(self, *uri, **params):
+        if len(uri) != 0:
+            if uri[0] == 'status':
+                value = random.randint(0, 1)
+            #if uri[0] == 'temp':
+                #value = random.randint(10, 25)
+            output = {'deviceID': self.deviceInfo['ID'], str(uri[0]): value}
+            return json.dumps(output)
+        else:
+            return json.dumps(self.deviceInfo)
+
+    def run(self):
+        while True:
+            time.sleep(self.pingInterval)
+            self.pingCatalog()
+    
+    def pingCatalog(self):
+        for device_id, device_info in self.deviceInfo.items():
+            requests.put(f'{self.catalogURL}/devices', data=json.dumps(device_info))
+
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True
+        }
+    }
+    s = SensorREST(5)
+    cherrypy.config.update({'server.socket_host': 'localhost', 'server.socket_port': 8081})
+    #cherrypy.config.update({'server.socket_port': 9090})
+    cherrypy.tree.mount(s, '/', conf)
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+    cherrypy.engine.exit()
---
+++ RESOURCE_CATALOG/catalog.py
@@ -0,0 +1,128 @@
+import cherrypy
+import json
+import time
+
+def addDevice(catalog, devicesInfo):
+    catalog["devices"].append(devicesInfo)
+
+def updateDevice(catalog, deviceID, devicesInfo):
+    for i in range(len(catalog["devices"])):
+        device = catalog["devices"][i]
+        if device['ID'] == deviceID:
+            catalog["devices"][i] = devicesInfo
+
+def removeDevices(catalog, deviceID):
+    for i in range(len(catalog["devices"])):
+        device = catalog["devices"][i]
+        if device['ID'] == int(deviceID):
+            catalog["devices"].pop(i)
+
+def addService(catalog, serviceInfo):
+    catalog["services"].append(serviceInfo)
+
+def updateService(catalog, serviceID, serviceInfo):
+    for i in range(len(catalog["services"])):
+        service = catalog["services"][i]
+        if service['ID'] == serviceID:
+            catalog["services"][i] = serviceInfo
+
+def removeService(catalog, seviceID):
+    for i in range(len(catalog["services"])):
+        service = catalog["services"][i]
+        if service['ID'] == int(seviceID):
+            catalog["services"].pop(i)
+
+class CatalogREST(object):
+    exposed = True
+
+    def __init__(self, catalog_address):
+        self.catalog_address = catalog_address
+    
+    def GET(self, *uri, **params):
+        catalog=json.load(open(self.catalog_address,"r"))
+        if len(uri)==0:#An error will be raised in case there is no uri 
+           raise cherrypy.HTTPError(status=400, message='UNABLE TO MANAGE THIS URL')
+        elif uri[0]=='all':
+            output = catalog
+        elif uri[0]=='devices':
+            output = {"devices":catalog["devices"]}
+        elif uri[0]=='services':
+            output = {"services":catalog["services"]}
+        return json.dumps(output)
+
+    def POST(self, *uri, **params):
+        catalog=json.load(open(self.catalog_address,"r"))
+        body = cherrypy.request.body.read()
+        json_body = json.loads(body.decode('utf-8'))
+        if uri[0]=='devices':
+            if not any(d['ID'] == json_body['ID'] for d in catalog["devices"]):
+                last_update = time.time()
+                json_body['last_update'] = last_update
+                addDevice(catalog, json_body)
+                output = f"Device with ID {json_body['ID']} has been added"
+                print(output)
+            else:
+                raise cherrypy.HTTPError(status=400, message='DEVICE ALREADY REGISTERED')
+        elif uri[0]=='services':
+            if not any(d['ID'] == json_body['ID'] for d in catalog["services"]):
+                last_update = time.time()
+                json_body['last_update'] = last_update
+                addService(catalog, json_body)
+                output = f"Service with ID {json_body['ID']} has been added"
+                print(output)
+            else:
+                raise cherrypy.HTTPError(status=400, message='SERVICE ALREADY REGISTERED')
+        json.dump(catalog,open(self.catalog_address,"w"),indent=4)
+        print(catalog)
+        return output
+
+
+    def PUT(self, *uri, **params):
+        catalog=json.load(open(self.catalog_address,"r"))
+        body = cherrypy.request.body.read()
+        json_body = json.loads(body.decode('utf-8'))
+        if uri[0]=='devices':
+            if not any(d['ID'] == json_body['ID'] for d in catalog["devices"]):
+                raise cherrypy.HTTPError(status=400, message='DEVICE NOT FOUND')
+            else:
+                last_update = time.time()
+                json_body['last_update'] = last_update
+                updateDevice(catalog, json_body['ID'], json_body)
+        elif uri[0]=='services':
+            if not any(d['ID'] == json_body['ID'] for d in catalog["services"]):
+                raise cherrypy.HTTPError(status=400, message='SERVICE NOT FOUND')
+            else:
+                last_update = time.time()
+                json_body['last_update'] = last_update
+                updateService(catalog, json_body['ID'], json_body)
+        print(catalog)
+        json.dump(catalog,open(self.catalog_address,"w"),indent=4)
+        return json_body
+        
+    def DELETE(self, *uri):
+        catalog=json.load(open(self.catalog_address,"r"))
+        if uri[0]=='devices':
+            removeDevices(catalog,uri[1])
+            output = f"Device with ID {uri[1]} has been removed"
+            print(output)
+        elif uri[0]=='services':
+            catalog = removeService(catalog,uri[1])
+            output = f"Service with ID {uri[0]} has been removed"
+            print(output)
+        json.dump(catalog,open(self.catalog_address,"w"),indent=4)
+
+
+if __name__ == '__main__':
+    catalogClient = CatalogREST("catalog.json")
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True
+        }
+    }
+    cherrypy.config.update({'server.socket_host': 'localhost', 'server.socket_port': 8080})
+    #cherrypy.config.update({'server.socket_port': 8080})
+    cherrypy.tree.mount(catalogClient, '/', conf)
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+    cherrypy.engine.exit()
